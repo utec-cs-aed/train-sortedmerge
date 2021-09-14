a6efd2db236188d314de9c7a47605f9317c7aee6
Heider Sanchez <48141762+heidersanchez@users.noreply.github.com>
2021-09-14 12:15:35 -0500

    fix solution.h

--- solution.h
+++ solution.h
@@ -11,21 +11,23 @@ void push_back(Node* &head, int val) {
     //TODO
 }
 
-// L1:  3 -> 5 -> 7 -> null
-//      t1
-// L2:  4 -> 5 -> 6 ->10 -> 15 -> null
-//      t2
-// L3:  3 -> 4 -> 5 -> 5 -> 6 -> 7 ->10 -> 15 -> null
+// L1:  3 -> 5 -> 7 -> null    O(n)
+//                      t1          
+// L2:  4 -> 5 -> 6 ->10 -> 15 -> null     O(m)
+//                                t2
+// opcion1
+// L3:  3 -> 4 -> 5 ->5 -> 6 -> 7 -> 10 -> 15 ->     O(n + m)
+//      L3                                 t3
+//opcion2
+// L3:  15 -> 10 -> 7-> 6-> 5-> 5-> 4-> 3->  O(1)
+// revert(L3)  O(n+m)      
+// L3:  3 -> 4 -> 5 ->5 -> 6 -> 7 -> 10 -> 15 ->
+// return L3
 
-Node* SortedMergeSeq(Node* l1, Node* l2) {
-    Node* l3 = nullptr;
-    //TODO
-    return l3;
-}
 
-Node* SortedMergeRec(Node* l1, Node* l2) {
-    //TODO
-    return nullptr;   
+Node* SortedMerge(Node* l1, Node* l2) {//O(n + m)
+   //TODO
+   throw ("Falta implementar");
 }
 
 void display(Node* head) {
